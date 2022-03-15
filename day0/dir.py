99706c759175346e25f5c02546d76fd910a10de3
Aleksei Fedorov <telesis@mail.ru>
2022-03-15 21:16:32 +0300

    Create dir.py

---
+++ day0/dir.py
@@ -0,0 +1,7 @@
+dir = "day1"
+for name in os.listdir(dir):
+     fullname = os.path.join(dir, name)
+     if os.path.isdir(fullname):
+             print("{} is a directory".format(fullname))
+     else:
+             print("{} is a file".format(fullname))

254460a0821c7766e2b693eb7b3b1a75acb8a78c
Aleksei Fedorov <telesis@mail.ru>
2022-03-17 00:19:10 +0300

    Update csv_pars.py

--- day0/csv_pars.py
+++ day0/csv_pars.py
@@ -2,6 +2,8 @@ import csv
 f = open("file.csv")
 #instance of CSV reader class
 csv_f = csv.reader(f)
+
 for row in csv_f:
     name, phone, role = row
     print("Name {}, Phone {}, Role {}".format(name, phone, role))
+f.close()
